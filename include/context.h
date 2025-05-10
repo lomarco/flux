4292bb5c883a6651e4986619ab2bd5fd26c3e19b
VorPijakov23 <ororr069@gmail.com>
2025-05-10 20:00:48 +0700

    feat: Add context.[c.h]

---
+++ include/context.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <stddef.h>
+
+typedef struct {
+  int argc;
+  char** argv;
+  char** env_vars;
+  size_t env_size;
+  int last_exit_code;
+} Context;
+
+Context* init_context(int argc, char* argv[]);
+
+void free_context(Context* ctx);
---
+++ src/context.c
@@ -0,0 +1,57 @@
+#include <context.h>
+#include <utils/error_utils.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+Context* init_context(int argc, char* argv[]) {
+  int i, j;
+
+  Context* ctx = (Context*)malloc(sizeof(Context));
+  if (!ctx) {
+    fprintf(stderr, "%s: error allocating Context\n", argv[0]);
+    return NULL;
+  }
+
+  ctx->env_size = 0;
+  ctx->last_exit_code = 0;
+  ctx->argc = argc;
+
+  ctx->argv = (char**)malloc((size_t)argc * sizeof(char*));
+  if (!ctx->argv) {
+    fprintf(stderr, "%s: error allocated argv\n", argv[0]);
+    free(ctx);
+    return NULL;
+  }
+  for (i = 0; i < argc; ++i) {
+    ctx->argv[i] = strdup(argv[i]);
+    if (!ctx->argv[i]) {
+      fprintf(stderr, "%s: error duplicating argv[%d]\n", argv[0], i);
+      for (j = 0; j < i; ++j)
+        free(ctx->argv[j]);
+      free(ctx->argv);
+      free(ctx);
+      return NULL;
+    }
+  }
+
+  ctx->argv[argc] = NULL;
+  return ctx;
+}
+
+void free_context(Context* ctx) {
+  int i;
+  size_t j;
+
+  if (!ctx)
+    return;
+  for (i = 0; i < ctx->argv; ++i)
+    free(ctx->argv[i]);
+  free(ctx->argv);
+
+  for (j = 0; j < ctx->env_size; ++j)
+    free(ctx->env_vars[j]);
+  free(ctx->env_vars);
+  free(ctx);
+}
